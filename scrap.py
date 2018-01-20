a41c6bca2e5c06db88d362ccc1ed18e94eed7aad
Nilesh Katuwal <nilesh.katuwal@gmail.com>
2018-01-21 02:01:56 +0545

    Writing extracted data to CSV file

--- scrap.py
+++ scrap.py
@@ -1,18 +1,24 @@
 from bs4 import BeautifulSoup
 import requests
+import csv
 
 source = requests.get('http://techprotricks.com').text
 soup = BeautifulSoup(source, 'html.parser')
-#print(soup.prettify())
-# t = soup.title.text
-# print(t)
-article = soup.find('article')
-# print(article.prettify())
-headline = article.h2.a.text
-cat = article.find('div', class_ = 'above-entry-meta').a.text
-pubtime = article.find('div', class_ = 'below-entry-meta').time.text
-summary = article.find('div', class_ = 'entry-content clearfix').p.text
-print(headline)
-print(cat)
-print(pubtime)
-print(summary)
+
+csv_file = open('techprotricks.csv','w')
+
+csv_writer = csv.writer(csv_file)
+csv_writer.writerow(['headline','Category','Published Date','Summary'])
+
+for article in soup.find_all('article'):
+
+   headline = article.h2.a.text
+   print(headline)
+   cat = article.find('div', class_='above-entry-meta').a.text
+   print(cat)
+   pubtime = article.find('div', class_='below-entry-meta').time.text
+   print(pubtime)
+   summary = article.find('div', class_='entry-content clearfix').p.text
+   print(summary)
+   csv_writer.writerow([headline, cat, pubtime, summary ])
+csv_file.close()
