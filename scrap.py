17196ccc3ed99e08837a25a51068da44ff8e97a2
Nilesh Katuwal <nilesh.katuwal@gmail.com>
2018-01-21 01:17:03 +0545

    Extracting text data from arcticle

--- scrap.py
+++ scrap.py
@@ -4,5 +4,13 @@ import requests
 source = requests.get('http://techprotricks.com').text
 soup = BeautifulSoup(source, 'html.parser')
 #print(soup.prettify())
-t = soup.title.text
-print(t)
+#t = soup.title.text
+#print(t)
+headline = article.h2.a.text
+cat = article.find('div', class_ = 'above-entry-meta').a.text
+pubtime = article.find('div', class_ = 'below-entry-meta').time.text
+summary = article.find('div', class_ = 'entry-content clearfix').p.text
+print(headline)
+print(cat)
+print(pubtime)
+print(summary)
